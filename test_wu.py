8367df904d80e4eaf2ae15b5dd9f370cc0057a46
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-05-04 08:43:46 +0800

    test commit

--- test_wu.py
+++ test_wu.py
@@ -1,4 +1,8 @@
 def test_truth():
     truth = True
     beauty = True
-    assert truth == beauty
+
+    if truth == beauty:
+        print("Truth = Beauty!")
+    else:
+        raise Exception("How can this be?!")
