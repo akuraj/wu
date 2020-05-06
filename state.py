aa387ad2f8059378bf011ba5ecb72e4e52355ec4
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2020-05-06 20:40:31 +0800

    print co-ordinates on side of board

--- state.py
+++ state.py
@@ -1,7 +1,7 @@
 import numpy as np
-from consts import (SIDE_LEN, EMPTY, BLACK, WHITE, WALL,
+from consts import (SIDE_LEN, SIDE_LEN_ACT, EMPTY, BLACK, WHITE, WALL,
                     ACT_ELEMS_TO_CHRS, ACT_ELEMS_TO_NAMES)
-from utils import new_board, search_board
+from utils import new_board, search_board, row_idx_to_num, col_idx_to_chr
 from pattern import P_WIN
 from enum import IntEnum, auto, unique
 
@@ -71,12 +71,33 @@ class State:
 
     def __repr__(self):
         board_repr = ""
+
         for i in range(self.board.shape[0]):
+            num_str = "  "
+            if 1 <= i <= SIDE_LEN_ACT:
+                num_str = str(row_idx_to_num(i))
+                if len(num_str) == 2:
+                    pass
+                elif len(num_str) == 1:
+                    num_str = " " + num_str
+                else:
+                    raise Exception(f"Invalid index: {i}!")
+
+            board_repr += num_str + " "
+
             for j in range(self.board.shape[1]):
                 board_repr += ACT_ELEMS_TO_CHRS[self.board[i][j]]
                 board_repr += " "
 
             board_repr += "\n"
+
+        board_repr += "     "
+
+        for i in range(1, SIDE_LEN_ACT + 1):
+            board_repr += col_idx_to_chr(i)
+            board_repr += " "
+
+        board_repr += "\n\n"
 
         return ("\nboard:\n{0}"
                 "turn: {1}\n"
